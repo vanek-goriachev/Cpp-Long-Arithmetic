af810d4647bfbfdff41b434bc509a589379493fe
lvanekl <83428939+lvanekl@users.noreply.github.com>
2024-01-28 00:35:27 +0300

    Написан конструктор из строки и метод для приведения к строке

--- main.cpp
+++ main.cpp
@@ -10,9 +10,61 @@ private:
     bool negative;       // Флаг отрицательности числа
     size_t precision;    // Точность после запятой
 
+    void normalize() {
+        // Удаляем лидирующие нули из целой части, кроме случая когда число равно 0
+        while (integerPart.length() > 1 && integerPart.front() == '0') {
+            integerPart.erase(integerPart.begin());
+        }
+
+        // Обрезаем или добавляем нули в дробную часть для достижения необходимой точности
+        if (decimalPart.length() > precision) {
+            decimalPart.erase(decimalPart.begin() + precision, decimalPart.end());
+        } else {
+            while (decimalPart.length() < precision) {
+                decimalPart.push_back('0');
+            }
+        }
+    }
+
 public:
     // Конструктор из строки
-    BigNumber(const string& number, size_t precision);
+    BigNumber(const string& number, size_t precision) : negative(false), precision(precision) {
+        string::const_iterator it = number.begin();
+
+        if (it != number.end() && (*it == '-' || *it == '+')) {
+            negative = (*it == '-');
+            ++it;
+        }
+
+        // Цикл до десятичной точки
+        while (it != number.end() && *it != '.') {
+            if (isdigit(*it)) {
+                integerPart.push_back(*it);
+            }
+            ++it;
+        }
+
+        // Если целая часть пустая, значит начало числа это 0
+        if (integerPart.empty()) {
+            integerPart = "0";
+        }
+
+        // Пропускаем десятичную точку
+        if (it != number.end() && *it == '.') {
+            ++it;
+        }
+
+        // Цикл после десятичной точки
+        while (it != number.end()) {
+            if (isdigit(*it)) {
+                decimalPart.push_back(*it);
+            }
+            ++it;
+        }
+
+        // Нормализация числа
+        normalize();
+    }
 
     // Конструктор из float
     BigNumber(float number, size_t precision);
@@ -21,7 +73,22 @@ public:
     BigNumber(double number, size_t precision);
 
     // Метод для получения строкового представления числа
-    string ToString() const;
+    string ToString() const {
+        string numberString;
+
+        if (negative && (integerPart != "0" || decimalPart != string(precision, '0'))) {
+            numberString += '-';
+        }
+
+        numberString += integerPart;
+
+        if (precision > 0) {
+            numberString += '.';
+            numberString += decimalPart;
+        }
+
+        return numberString;
+    };
 
     // Перегрузка арифметических операторов
     BigNumber operator+(const BigNumber& other) const;
@@ -38,9 +105,6 @@ public:
     bool operator<(const BigNumber& other) const;
     bool operator>(const BigNumber& other) const;
 
-    // Деструктор
-    ~BigNumber();
-
     // Дополнительные методы могут быть добавлены здесь, например:
     // - методы для внутренней нормализации числа
     // - операторы +=, -=, *=, /=
@@ -48,6 +112,8 @@ public:
 };
 
 int main() {
-    cout << "Hello world" << endl;
+    BigNumber n1("12.32", 5);
+
+    cout << n1.ToString() << endl;
     return 0;
 }
