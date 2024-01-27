6da5a8a3bc0e7d84ca5cce51b4957764e61f60bb
lvanekl <83428939+lvanekl@users.noreply.github.com>
2024-01-28 01:19:34 +0300

    Реализована перегрузка оператора унарный минус и проведен рефакторинг кода

--- main.cpp
+++ main.cpp
@@ -4,60 +4,82 @@
 
 using namespace std;
 
-class BigNumber {
+class BigNumber
+{
 private:
     string integerPart;  // Целая часть числа
     string decimalPart;  // Десятичная часть числа (до заданной точности)
     bool negative;       // Флаг отрицательности числа
     size_t precision;    // Точность после запятой
 
-    void normalize() {
+    void normalize()
+    {
         // Удаляем лидирующие нули из целой части, кроме случая когда число равно 0
-        while (integerPart.length() > 1 && integerPart.front() == '0') {
+        while (integerPart.length() > 1 && integerPart.front() == '0')
+        {
             integerPart.erase(integerPart.begin());
         }
 
         // Обрезаем или добавляем нули в дробную часть для достижения необходимой точности
-        if (decimalPart.length() > precision) {
+        if (decimalPart.length() > precision)
+        {
             decimalPart.erase(decimalPart.begin() + precision, decimalPart.end());
-        } else {
-            while (decimalPart.length() < precision) {
+        } else
+        {
+            while (decimalPart.length() < precision)
+            {
                 decimalPart.push_back('0');
             }
         }
     }
 
 public:
+    // Конструктор из BigNumber
+    BigNumber(const BigNumber &other) : negative(other.negative), precision(other.precision),
+                                        integerPart(other.integerPart), decimalPart(other.decimalPart)
+    {
+        // Нормализация числа
+        normalize();
+    }
+
     // Конструктор из строки
-    BigNumber(const string& number, size_t precision) : negative(false), precision(precision) {
+    BigNumber(const string &number, size_t precision) : negative(false), precision(precision)
+    {
         string::const_iterator it = number.begin();
 
-        if (it != number.end() && (*it == '-' || *it == '+')) {
+        if (it != number.end() && (*it == '-' || *it == '+'))
+        {
             negative = (*it == '-');
             ++it;
         }
 
         // Цикл до десятичной точки
-        while (it != number.end() && *it != '.') {
-            if (isdigit(*it)) {
+        while (it != number.end() && *it != '.')
+        {
+            if (isdigit(*it))
+            {
                 integerPart.push_back(*it);
             }
             ++it;
         }
 
         // Если целая часть пустая, значит начало числа это 0
-        if (integerPart.empty()) {
+        if (integerPart.empty())
+        {
             integerPart = "0";
         }
 
         // Пропускаем десятичную точку
-        if (it != number.end() && *it == '.') {
+        if (it != number.end() && *it == '.')
+        {
             ++it;
         }
 
         // Цикл после десятичной точки
-        while (it != number.end()) {
-            if (isdigit(*it)) {
+        while (it != number.end())
+        {
+            if (isdigit(*it))
+            {
                 decimalPart.push_back(*it);
             }
             ++it;
@@ -68,9 +90,11 @@ public:
     }
 
     // Конструктор из float
-    BigNumber(float number, size_t prec) : precision(prec), negative(number < 0) {
+    BigNumber(float number, size_t prec) : precision(prec), negative(number < 0)
+    {
         // Используем тернарный оператор, чтобы установить значение флага отрицательности
-        if (number < 0) {
+        if (number < 0)
+        {
             number = -number; // Работаем с числом как с положительным
         }
 
@@ -91,12 +115,14 @@ public:
         decimalPart = to_string(decimalPartAsInt);
 
         // Дополняем дробную часть нулями, если она короче нужной точности
-        while (decimalPart.length() < precision) {
+        while (decimalPart.length() < precision)
+        {
             decimalPart = "0" + decimalPart;
         }
 
         // Если округление дало нам "1000" в decimalPart при precision 3, необходимо перенести '1' в integerPart
-        if (decimalPart.length() > precision) {
+        if (decimalPart.length() > precision)
+        {
             // Добавляем переносимый '1' к целой части
             integerPart = to_string(integerPartNumber + 1);
 
@@ -106,9 +132,11 @@ public:
     }
 
     // Конструктор из double
-    BigNumber(double number, size_t prec) : precision(prec), negative(number < 0) {
+    BigNumber(double number, size_t prec) : precision(prec), negative(number < 0)
+    {
         // Используем тернарный оператор, чтобы установить значение флага отрицательности
-        if (number < 0) {
+        if (number < 0)
+        {
             number = -number; // Работаем с числом как с положительным
         }
 
@@ -129,12 +157,14 @@ public:
         decimalPart = to_string(decimalPartAsInt);
 
         // Дополняем дробную часть нулями, если она короче нужной точности
-        while (decimalPart.length() < precision) {
+        while (decimalPart.length() < precision)
+        {
             decimalPart = "0" + decimalPart;
         }
 
         // Если округление дало нам "1000" в decimalPart при precision 3, необходимо перенести '1' в integerPart
-        if (decimalPart.length() > precision) {
+        if (decimalPart.length() > precision)
+        {
             // Добавляем переносимый '1' к целой части
             integerPart = to_string(integerPartNumber + 1);
 
@@ -144,16 +174,19 @@ public:
     }
 
     // Метод для получения строкового представления числа
-    string ToString() const {
+    string ToString() const
+    {
         string numberString;
 
-        if (negative && (integerPart != "0" || decimalPart != string(precision, '0'))) {
+        if (negative && (integerPart != "0" || decimalPart != string(precision, '0')))
+        {
             numberString += '-';
         }
 
         numberString += integerPart;
 
-        if (precision > 0) {
+        if (precision > 0)
+        {
             numberString += '.';
             numberString += decimalPart;
         }
@@ -162,19 +195,32 @@ public:
     };
 
     // Перегрузка арифметических операторов
-    BigNumber operator+(const BigNumber& other) const;
-    BigNumber operator-(const BigNumber& other) const;
-    BigNumber operator*(const BigNumber& other) const;
-    BigNumber operator/(const BigNumber& other) const;
+    BigNumber operator+(const BigNumber &other) const;
+    BigNumber operator-(const BigNumber &other) const;
+
+    BigNumber operator*(const BigNumber &other) const;
+
+    BigNumber operator/(const BigNumber &other) const;
+
+    // Перегрузка оператора унарный минус
+    BigNumber operator-() const
+    {
+        BigNumber result(*this);
+        result.negative = !result.negative;
+        return result;
+    };
 
     // Перегрузка оператора присваивания
-    BigNumber& operator=(const BigNumber& other);
+    BigNumber &operator=(const BigNumber &other);
 
     // Перегрузка операторов сравнения
-    bool operator==(const BigNumber& other) const;
-    bool operator!=(const BigNumber& other) const;
-    bool operator<(const BigNumber& other) const;
-    bool operator>(const BigNumber& other) const;
+    bool operator==(const BigNumber &other) const;
+
+    bool operator!=(const BigNumber &other) const;
+
+    bool operator<(const BigNumber &other) const;
+
+    bool operator>(const BigNumber &other) const;
 
     // Дополнительные методы могут быть добавлены здесь, например:
     // - методы для внутренней нормализации числа
@@ -182,15 +228,15 @@ public:
     // - операторы сравнения <=, >=
 };
 
-int main() {
-    BigNumber n1("-12.01234567890123456789", 10);
+int main()
+{
+    BigNumber n1("-12.01234567890123456789", 5);
     cout << n1.ToString() << endl;
 
     BigNumber n2(float(-12.0123456789), 5);
     cout << n2.ToString() << endl;
 
-    BigNumber n3(-12.01234567890123456789, 12);
+    BigNumber n3(-0990912.01235, 5);
     cout << n3.ToString() << endl; // из численного значения перевод работает не слишком хорошо
-    cout << -12.01234567890123456789 << endl;
     return 0;
 }
